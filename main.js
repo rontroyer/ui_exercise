4040553f096305ede26257223efcc631b63f7bbc
unknown <rtroyer@INFOMGMT-PC5.OCCRRA.local>
2015-08-04 10:06:22 -0400

    Remove and sort functions connected.

--- main.js
+++ main.js
@@ -18,13 +18,39 @@ app = {
 			app.newProvider('['+jsonString+']');
 			$(this).find('input[type="text"]').val('');
 		});
+
+		$('.provider_list').on('click','.provider', function() {
+			$(this).toggleClass('checked');
+			$('.remove_provider .count').text($('.provider.checked').length == 0 ? '' : ' '+$('.provider.checked').length + ' provider'+($('.provider.checked').length>1 ? 's' : ''));
+		});
 	},
 	insertDefault : function () {
 		var dummyJSON = '[{"last_name": "Harris", "first_name": "Mike", "email_address": "mharris@updox.com", "specialty": "Pediatrics", "practice_name": "Harris Pediatrics"}, {"last_name": "Wijoyo", "first_name": "Bimo", "email_address": "bwijoyo@updox.com", "specialty": "Podiatry", "practice_name": "Wijoyo Podiatry"}, {"last_name": "Rose", "first_name": "Nate", "email_address": "nrose@updox.com", "specialty": "Surgery", "practice_name": "Rose Cutters"}, {"last_name": "Carlson", "first_name": "Mike", "email_address": "mcarlson@updox.com", "specialty": "Orthopedics", "practice_name": "Carlson Orthopedics"}, {"last_name": "Witting", "first_name": "Mike", "email_address": "mwitting@updox.com", "specialty": "Pediatrics", "practice_name": "Witting’s Well Kids Pediatrics"}, {"last_name": "Juday", "first_name": "Tobin", "email_address": "tjuday@updox.com", "specialty": "General Medicine", "practice_name": "Juday Family Practice"} ]';
 		app.newProvider(dummyJSON);
 	},
 	prepareSort : function () {
-		
+		/* Popupulate sort */
+		$('#sort_by').append('<option disabled selected>Sort By</option>');
+		$('#provider_form label').each(function() {
+			n = $(this).attr('for');
+			t = $(this).text();
+			$('#sort_by').append('<option value="'+n+'">'+t+'</option>');
+		});
+
+		/* Add event listener to sort */
+		$('#sort_by').change(function() {
+			sort_by = $(this).val();
+			$('.provider').sort(function(a, b) {
+				console.log(sort_by);
+	            if ($(a).find('.'+sort_by).text() < $(b).find('.'+sort_by).text()) {
+	                return -1;
+	            } else if ($(a).find('.'+sort_by).text() > $(b).find('.'+sort_by).text()) {
+	                return 1;
+	            } else {
+	                return 0;
+	            }
+	        }).detach().appendTo('.provider_list');
+		});
 	},
 	newProvider : function (jsonString) {
 		/* Convert JSON to array */
